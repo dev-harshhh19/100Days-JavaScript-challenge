230a69ddd5b83e05652c91785428c7fbcb1ddb4c
dev-harshhh19 <harshadnikam7516@gmail.com>
2025-11-21 23:16:02 +0530

    Add DOM manipulation examples with console logging

---
+++ Day_41/DOM.js
@@ -0,0 +1,16 @@
+console.log(document); // This will log the entire document object
+
+// Accessing the title of the document
+console.log("Document Title:", document.title);
+
+// ---------------------------
+
+// getElementById
+const elementById = document.getElementById('message');
+console.log(elementById)
+
+// ---------------------------
+
+// querySelector
+const elementByQuery = document.querySelector(".unique-heading");
+console.log(elementByQuery);
